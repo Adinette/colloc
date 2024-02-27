f6f3abd27d64b2992cc4b1f3b76df14eec99da41
Aymar Sossou <saymar90@gmail.com>
2024-02-27 14:50:41 +0100

    Update code.

--- js/sidebar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Gérer le clic sur le SVG
-    $("#toggleSidebar").click(function () {
-        // Montrer ou cacher le sidebar en fonction de son état actuel
-        $("#sidebar").toggleClass("d-none");
-    });
-});
