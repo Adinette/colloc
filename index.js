30428a66f8960fc1a5fc4e720a7b3f2da409fa77
Adinette Dougloui <adinettedougloui@gmail.com>
2024-03-04 06:05:04 -1100

    add page catalogue and login

---
+++ index.js
@@ -0,0 +1,32 @@
+$('.responsive').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2.5
+        }
+      }
+    ]
+  });
